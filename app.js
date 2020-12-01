33fa15ea553ea5dc3e272ddfa2aa5c2d525f4936
zhanghao12 <zhanghao12@kuaishou.com>
2020-12-01 20:25:53 +0800

    增加了云存储功能 & 优化了项目展示

--- app.js
+++ app.js
@@ -1,39 +1,83 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+	onLaunch: function () {
+		// 展示本地存储能力
+		var logs = wx.getStorageSync('logs') || []
+		logs.unshift(Date.now())
+		wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+		wx.cloud.init()
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
-  }
+		let app = this;
+
+		// 登录
+		wx.login({
+			success: res => {
+				if (res.code) {
+					//获取openId
+					wx.request({
+						url: 'https://api.weixin.qq.com/sns/jscode2session',
+						data: {
+							//小程序唯一标识
+							appid: 'wx07cb15f4e9757dc6',
+							//小程序的 app secret
+							secret: '5fd3874b2dcb0c125c6ccc69ffb04887',
+							grant_type: 'authorization_code',
+							js_code: res.code
+						},
+						method: 'GET',
+						header: {
+							'content-type': 'application/json'
+						},
+						success: function (openIdRes) {
+							console.info("登录成功返回的openId：" + openIdRes.data.openid);
+							app.globalData.openId = openIdRes.data.openid;
+							// 判断openId是否获取成功
+							if (openIdRes.data.openid != null & openIdRes.data.openid != undefined) {
+								// 有一点需要注意 询问用户 是否授权 那提示 是这API发出的
+								wx.getUserInfo({
+									success: function (data) {
+										// 自定义操作
+										// 绑定数据，渲染页面
+									},
+									fail: function (failData) {
+										console.info("用户拒绝授权");
+									}
+								});
+							} else {
+								console.info("获取用户openId失败");
+							}
+						},
+						fail: function (error) {
+							console.info("获取用户openId失败");
+							console.info(error);
+						}
+					})
+				}
+			}
+		})
+		// 获取用户信息
+		wx.getSetting({
+			success: res => {
+				if (res.authSetting['scope.userInfo']) {
+					// 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+					wx.getUserInfo({
+						success: res => {
+							// 可以将 res 发送给后台解码出 unionId
+							this.globalData.userInfo = res.userInfo
+
+							// 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+							// 所以此处加入 callback 以防止这种情况
+							if (this.userInfoReadyCallback) {
+								this.userInfoReadyCallback(res)
+							}
+						}
+					})
+				}
+			}
+		})
+	},
+	globalData: {
+		openId: null
+	}
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,95 +1,170 @@
 //index.js
+function getToday(){
+	let today = new Date()
+	return today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
+}
 
 Page({
-  data: {
-    pname:'',
-    pmny:'',
-    curType:0, // 收入/支出
-    placeholder:'请输入收入金额',
-    btnname:"切为支出",
-    projects : [],
-    date: "2019-01-01",
-    countryCodes: ["1个月", "2个月", "3个月", "4个月", "5个月", "6个月", "7个月", "8个月", "9个月", "10个月", "11个月", "12个月"],
-    countryCodeIndex: 0,
-    checkboxItems: [
-      { name: '支出', value: '0', checked: true }
-    ],
-  },
-  bindCountryCodeChange: function (e) {
-    this.setData({
-      countryCodeIndex: e.detail.value
-    })
-  },
-  //事件处理函数
-  bindViewTap: function() {
-   
-  },
-  onLoad: function () {
-    
-  },
-  getUserInfo: function(e) {
-    
-  },
-  bindDateChange: function (e) {
-    this.setData({
-      date: e.detail.value
-    })
-  },
-  addProject: function(e){
-    let project = {
-      "name":e.detail.value.name,
-      "cycle": parseInt(e.detail.value.cycle),
-      "cycleBase": e.detail.value.cycleBase,
-      "mny": parseInt(e.detail.value.mny),
-    };
+	data: {
+		pname: '', //用户输入的
+		pmny: '', //用户输入的收支金额
+		curType: 0, // 收入指出输入框目前的状态 （0=收入，1=支出）
+		placeholder: '请输入收入金额', //收支输入框的palcehodler
+		btnname: "切为支出", //收支输入框前面的按钮名称（切为支出/切为收入）
+		projects: [], //用户添加完成的的项目数组
+		date: getToday(), //用户选择的项目开始日期
+		countryCodes: ["1个月", "2个月", "3个月", "4个月", "5个月", "6个月", "7个月", "8个月", "9个月", "10个月", "11个月", "12个月"], //周期枚举
+		countryCodeIndex: 0, //用户选择的周期索引
+	},
+	bindCountryCodeChange: function (e) {
+		this.setData({
+			countryCodeIndex: e.detail.value
+		})
+	},
+	//"添加项目"按钮触发的方法
+	addProject: function (e) {
+		let project = {
+			"name": e.detail.value.name,
+			"cycle": parseInt(e.detail.value.cycle)+1,
+			"cycleBase": e.detail.value.cycleBase,
+			"mny": parseInt(e.detail.value.mny),
+		};
 
-    if (project.name && project.cycle && project.cycleBase && project.mny){
-      this.data.projects.push(project)
-      this.setData({ "projects":this.data.projects})
-      console.log(this.data.projects)
-      this.clear()
-    }else{
-      wx.showModal({
-        content: '缺少输入值',
-        showCancel: false,
-        success: function (res) {
-          
-        }
-      });
-    }    
-  },
-  remove : function(e){
-    this.data.projects.splice(e.currentTarget.dataset.index,1);
-    this.setData({ "projects": this.data.projects })
-  },
-  clear : function(){
-    this.setData({"pname":"","pmny":""})
-  },
-  goto:function(){
-    // 跳转之前，将项目集合保存下来
-    wx.setStorageSync('projects', JSON.stringify(this.data.projects))
-    wx.navigateTo({
-      url: '../main/main'
-    })
-  },
-  toggle:function(){
-    if(this.data.curType == 0){
-      this.setData({ "curType": 1,"placeholder":"请输入支出金额","btnname":"切为收入"})
-      if (this.data.pmny) {
-        this.setData({ "pmny": -this.data.pmny })
-      }
-    }else{
-      this.setData({ "curType": 0, "placeholder": "请输入收入金额", "btnname": "切为支出" })
-      if (this.data.pmny){
-        this.setData({ "pmny": Math.abs(this.data.pmny) })
-      }
-    }
-  },
-  changeinput : function(e){
-    this.setData({ "pmny": e.detail.value })
-    if (this.data.curType == 1) {
-      let mny = parseInt(Math.abs(e.detail.value))
-      this.setData({"pmny":-mny})
-    }
-  }
+		if (project.name && project.cycle && project.cycleBase && project.mny) {
+			this.data.projects.push(project)
+			this.setData({
+				"projects": this.data.projects
+			})
+			console.log(this.data.projects)
+			this.clear()
+			this.saveList();
+		} else {
+			wx.showModal({
+				content: '缺少输入值',
+				showCancel: false,
+				success: function (res) {
+
+				}
+			});
+		}
+	},
+	// 删除按钮触发
+	remove: function (e) {
+		this.data.projects.splice(e.currentTarget.dataset.index, 1);
+		this.setData({
+			"projects": this.data.projects
+		})
+		this.saveList();
+	},
+	clear: function () {
+		this.setData({
+			"pname": "",
+			"pmny": ""
+		})
+	},
+	// 点击查看结果按钮触发
+	goto: function () {
+		// 跳转之前，将项目集合保存下来
+		wx.setStorageSync('projects', JSON.stringify(this.data.projects))
+		wx.navigateTo({
+			url: '../main/main'
+		})
+	},
+	//点击切换收入/支出按钮触发
+	toggle: function () {
+		if (this.data.curType == 0) {
+			this.setData({
+				"curType": 1,
+				"placeholder": "请输入支出金额",
+				"btnname": "切为收入"
+			})
+			if (this.data.pmny) {
+				this.setData({
+					"pmny": -this.data.pmny
+				})
+			}
+		} else {
+			this.setData({
+				"curType": 0,
+				"placeholder": "请输入收入金额",
+				"btnname": "切为支出"
+			})
+			if (this.data.pmny) {
+				this.setData({
+					"pmny": Math.abs(this.data.pmny)
+				})
+			}
+		}
+	},
+	changeinput: function (e) {
+		this.setData({
+			"pmny": e.detail.value
+		})
+		if (this.data.curType == 1) {
+			let mny = parseInt(Math.abs(e.detail.value))
+			this.setData({
+				"pmny": -mny
+			})
+		}
+	},
+	saveList: function (e) {
+		let pj = this.data.projects
+
+		let db = wx.cloud.database()
+		let col = db.collection('test')
+		col.where({_openid:getApp().globalData.openId}).get().then(res => {
+			if(res.data && res.data.length > 0){
+				console.log('1')
+				col.doc(res.data[0]['_id']).update({
+					data: {
+						projects : pj
+					},
+					success: function (res) {
+						// wx.showToast({
+						//   title: '保存成功！',
+						// })
+					}
+				})
+			}else{
+				console.log('2')
+				col.add({
+					data: {
+						projects : pj
+					},
+					success: function (res) {
+						// wx.showToast({
+						// 	title: '保存成功！',
+						//   })
+					}
+				})
+			}
+		});
+	},
+	//事件处理函数
+	bindViewTap: function () {
+
+	},
+	onLoad: function () {
+		console.log('123')
+		let db = wx.cloud.database()
+		let indexThis = this;
+		db.collection('test').where({_openid:getApp().globalData.openId}).get().then(res => {
+			console.log(res)
+			indexThis.setData({
+				"projects": res.data[0].projects
+			})
+		})
+	},
+	getUserInfo: function (e) {
+
+	},
+	onPullDownRefresh: function(){
+		this.onLoad();
+		wx.stopPullDownRefresh()
+	},
+	bindDateChange: function (e) {
+		this.setData({
+			date: e.detail.value
+		})
+	}
 })
